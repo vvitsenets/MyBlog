e2e3dac2ddbc0d06fc38ed0943c41b8402f116d1
Vladislav Vitsenets <mr.brendon@gmail.com>
2021-02-18 14:26:45 +0200

    masterbrunch update ua lang

--- js/google-translate.js
+++ js/google-translate.js
@@ -6,7 +6,7 @@
 
 const googleTranslateConfig = {
     /* Original language */
-    lang: "ru",
+    lang: "ua",
     /* The language we translate into on the first visit*/
     /* Язык, на который переводим при первом посещении */
     // langFirstVisit: 'en',
